0d3437b7fce3a7e329941e306b86605e000504c9
Daniel <yhchen518@gamil.com>
2021-04-03 16:10:39 -0500

    Modified sensitive content

--- maint/settings.py
+++ maint/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-hq*zddgn1&&3a+r5g1%e=e@0ruu$b24@une2$z#*8$vgl)m5@'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
